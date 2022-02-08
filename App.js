7f5fae069ee2a7e284320027fe078ed52b7361cf
fuatsevinc <fuatsevinc@icloud.com>
2022-02-08 20:00:05 +0100

    app.js page updated

--- App.js
+++ App.js
@@ -1,42 +1,27 @@
-/* camel case theQuickBrownFoxJumpsOverTheLazyDog
-
-var printCustomerMessage = 'Welcome Customer'
-
-snake case "the_quick_brown_fox_jumps_over_the_lazy_dog"
-
-kebab case or Train case "the-quick-brown-fox-jumps-over-the-lazy-dog" 
-
-screaming snake case (or SCREAMING_SNAKE_CASE) or hazard case. */
-
-// var x = 999999999999999;     
-// var y = 9999999999999999;
-
-// console.log(x);
-// console.log(y);
-
-// var z = 1.12345678912345678
-// var zz = 1.12345678912345678455
-
-// console.log(z);
-// console.log(zz);
-
-// console.log(Number.MAX_SAFE_INTEGER); //9007199254740991
-
-
-// var x = 0.1 + 0.2
-// console.log(x);
-// console.log(+x.toFixed(2));
-
-
-// console.log(15+25);
-// console.log(0000015+00025); //octal 01234567 015 : 5 + 8 = 13 025: 5 + 16 = 21 13+21 =34
-// console.log(018+028); //decimal
-
-// console.log(Number(018));
-
-let a = 2;
-let b = a;
-console.log({a},{b});
-
- a = 3;
-console.log({a},{b});
+function fibonacci(n) {
+    let [a, b] = [0, 1]
+    const res = []
+​
+    while (b < n){
+        res.push(b);
+        [a, b] = [b, a+b]
+    }
+    return res;
+}
+​
+console.log(fibonacci(15))
+  
+//
+​
+const number = prompt("enter a number for fibonacci:");
+var num1 = 0;
+var num2 = 1;
+var result;
+for (let i = 1; i <= number; i++) {
+    console.log(num1);
+    result = num1 + num2;
+    num1 = num2;
+    num2 = result;
+}
+console.log(result);
+Geteilt in
